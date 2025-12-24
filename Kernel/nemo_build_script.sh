7f49ab6349c17d0cf7f650f0fef304d30a96c44f
αиѕн <singhansh64321@gmail.com>
2025-12-24 17:31:17 +0530

    scripts: kernel:/nemo : Rename zip file to Stormbreaker-LTS

--- Kernel/nemo_build_script.sh
+++ Kernel/nemo_build_script.sh
@@ -129,7 +129,7 @@ compile() {
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Stormbreaker-Test-KSU-RUI2-KERNEL-"${CODENAME}"-"${DATE}".zip ./*
+    zip -r9 Stormbreaker-LTS-Test-RUI2-KERNEL-"${CODENAME}"-"${DATE}".zip ./*
     cd ..
 }
 
